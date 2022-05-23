16624ea7a29ba6f6d7db453e92466b88624475bc
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2022-05-23 08:51:38 -0600

    Add deploy

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const ErrorBoundary = ({ children }) => {
+  const [hasError, setError] = useState(false);
+
+  return;
+}
+
+export default ErrorBoundary;
